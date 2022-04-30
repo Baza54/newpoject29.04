d9a9899fbbc7dfc55c2edc6c65c9db9cee4125d2
Baza54 <nerf.king.neon@mail.ru>
2022-04-30 14:02:09 +0600

    commit 30.04

---
+++ app/src/main/java/com/example/newpoject2904/tableactivity.kt
@@ -0,0 +1,11 @@
+package com.example.newpoject2904
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tableactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tableactivity)
+    }
+}
